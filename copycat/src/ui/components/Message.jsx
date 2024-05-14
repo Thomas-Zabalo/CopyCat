d99305970dd2bb3361cd8fae58baf0c7cb577db1
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-05-14 16:12:21 +0200

    Add Messsage informatif

--- copycat/src/ui/components/Message.jsx
+++ copycat/src/ui/components/Message.jsx
@@ -4,21 +4,21 @@ import React from 'react';
 
 export default function Message() {
     const close = () => {
-        const closeButton = document.getElementsByTagName("close");
-        if (closeButton) {
-            const hidden = document.getElementById("hidden");
+        const hidden = document.getElementById("hidden");
+        if (hidden) {
             hidden.style.display = "none";
         }
     };
 
     return (
-        <div id="hidden" className="flex items-center justify-between py-3 px-3 bg-gray-100">
-            <div className="font-semibold justify-center">
+        <div id="hidden" className="relative flex items-center justify-center py-3 px-3 bg-gray-100">
+            <div className="font-semibold text-center">
                 Notre entrepôt fait peau neuve pour mieux vous servir. Pendant cette période, les livraisons peuvent être légèrement retardées.
             </div>
-            <button onClick={close}>
-                <XCircleIcon className="h-6 w-6 text-black hover:text-gray-600 absolute" />
+            <button onClick={close} className="absolute right-0 mr-5">
+                <XCircleIcon className="h-6 w-6 text-black hover:text-gray-600" />
             </button>
         </div>
     );
 }
+
