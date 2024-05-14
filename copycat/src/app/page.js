ee77a4d31978f03843661f8fcc5e016a150880ae
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-05-14 16:15:24 +0200

    Adjadjustment page

--- copycat/src/app/page.js
+++ copycat/src/app/page.js
@@ -1,13 +1,34 @@
 import Image from "next/image";
 
-
 export default function Home() {
   return (
     <div>
-     
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="bg-image">
+        <div className="pt-24 pl-7">
+          <h1 className="text-5xl leading-relaxed">
+            La multi-activité,
+            <br />
+            au cœur de notre collection
+            <br />
+            printemps-été 2024
+          </h1>
+          <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+
+            <button class="text-white bg-black group rounded-full border border-transparent px-2 py-3 transition-colors hover:bg-orange-600 hover:dark:bg-orange-600 m-5">
+              Nouvelle collection
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none px-2">
+                -&gt;
+              </span>
+            </button>
+
+
+          </div>
+        </div>
+      </div>
+
+      <main className="">
 
       </main>
-    </div>
+    </div >
   );
 }
