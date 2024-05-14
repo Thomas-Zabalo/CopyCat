f2106770d79b9edff5e0dcb5714e1cb72ff1a0bd
Thomas ZABALO <thomas.zabalo@gmail.com>
2024-05-14 17:41:20 +0200

    Change message

--- copycat/src/ui/components/Message.jsx
+++ copycat/src/ui/components/Message.jsx
@@ -12,8 +12,10 @@ export default function Message() {
 
     return (
         <div id="hidden" className="relative flex items-center justify-center py-3 px-3 bg-gray-100">
-            <div className="font-semibold text-center">
-                Notre entrepôt fait peau neuve pour mieux vous servir. Pendant cette période, les livraisons peuvent être légèrement retardées.
+            <div className=" font-normal text-center">
+                <p>
+                    Notre entrepôt fait peau neuve pour mieux vous servir. Pendant cette période, les livraisons peuvent être légèrement retardées.
+                </p>
             </div>
             <button onClick={close} className="absolute right-0 mr-5">
                 <XCircleIcon className="h-6 w-6 text-black hover:text-gray-600" />
